f5787b8e712ee50ac5f7322bb92dd84ef29de819
morshedul-antor <antor.morshedul@gmail.com>
2024-05-07 23:13:57 +0600

    fix: jwt token header algorithm

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const createToken = async (id) => {
     const token = jwt.sign({ sid: id }, process.env.JWT_SECRET, {
         expiresIn: '48h',
+        algorithm: 'HS256',
     })
 
     return { token: token, type: 'bearer' }
