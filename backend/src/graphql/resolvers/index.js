5e26aa7f253cb62ba9a96cea4594656489822317
morshedul-antor <antor.morshedul@gmail.com>
2024-05-08 01:32:42 +0600

    import: category resolvers queries and mutations

--- backend/src/graphql/resolvers/index.js
+++ backend/src/graphql/resolvers/index.js
@@ -1,12 +1,15 @@
 const { userResolvers } = require('./userResolvers')
+const { categoryResolvers } = require('./categoryResolvers')
 
 const resolvers = {
     Query: {
         ...userResolvers.Query,
+        ...categoryResolvers.Query,
     },
 
     Mutation: {
         ...userResolvers.Mutation,
+        ...categoryResolvers.Mutation,
     },
 }
 
