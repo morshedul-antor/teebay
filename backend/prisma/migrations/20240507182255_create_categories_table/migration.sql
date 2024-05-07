21feeb159281e37bee0a8c47c9167ee8a1a8e219
morshedul-antor <antor.morshedul@gmail.com>
2024-05-08 01:26:24 +0600

    build: category table migration add

---
+++ backend/prisma/migrations/20240507182255_create_categories_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
