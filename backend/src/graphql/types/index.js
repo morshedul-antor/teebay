35f300fb8b4f5dccf01ec941cf214c2be215f081
morshedul-antor <antor.morshedul@gmail.com>
2024-05-08 13:57:08 +0600

    import: product type defs queries and mutatations

--- backend/src/graphql/types/index.js
+++ backend/src/graphql/types/index.js
@@ -1,9 +1,11 @@
 const { userTypeDefs } = require('./userTypes')
 const { categoryTypeDefs } = require('./categoryTypes')
+const { productTypeDefs } = require('./productTypes')
 
 const typeDefs = `
     ${userTypeDefs}
     ${categoryTypeDefs}
+    ${productTypeDefs}
 `
 
 module.exports = {
