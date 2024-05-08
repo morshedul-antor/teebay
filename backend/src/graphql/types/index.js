266eb8309842edf1c8ca7f8ea3c28d2ba58059ae
morshedul-antor <antor.morshedul@gmail.com>
2024-05-08 18:38:17 +0600

    import: order type defs queries and mutation

--- backend/src/graphql/types/index.js
+++ backend/src/graphql/types/index.js
@@ -1,11 +1,13 @@
 const { userTypeDefs } = require('./userTypes')
 const { categoryTypeDefs } = require('./categoryTypes')
 const { productTypeDefs } = require('./productTypes')
+const { orderTypeDefs } = require('./orderTypes')
 
 const typeDefs = `
     ${userTypeDefs}
     ${categoryTypeDefs}
     ${productTypeDefs}
+    ${orderTypeDefs}
 `
 
 module.exports = {
