3b462d11415fa1b7d97816023ea44a91627aed85
morshedul-antor <antor.morshedul@gmail.com>
2024-05-09 01:28:55 +0600

    feat: order resolver's orders by user queries add with type filter sold, bought, rent, borrowed and lent

--- backend/src/graphql/resolvers/orderResolvers.js
+++ backend/src/graphql/resolvers/orderResolvers.js
@@ -27,6 +27,52 @@ const orderResolvers = {
                 throw new Error(error.message)
             }
         },
+
+        ordersByUser: async (_, { userId, type }) => {
+            let filter = { orderType: 'trade' }
+
+            // type: sold, bought, rent, borrow and lent
+            if (type === 'sold') {
+                filter.providerId = userId
+            } else if (type === 'bought') {
+                filter.recipientId = userId
+            } else if (type === 'rent' || type === 'borrow') {
+                filter = {
+                    orderType: type === 'rent' ? 'loan' : 'borrow',
+                    recipientId: userId,
+                }
+            } else {
+                filter = {
+                    orderType: 'loan' || 'borrow',
+                    providerId: userId,
+                }
+            }
+
+            try {
+                const orders = await prismaClient.order.findMany({
+                    where: filter,
+                    include: {
+                        product: true,
+                    },
+                })
+
+                for (const order of orders) {
+                    const product = order.product
+                    const categories = await prismaClient.category.findMany({
+                        where: {
+                            id: {
+                                in: product.categoryIds,
+                            },
+                        },
+                    })
+                    product.categories = categories
+                }
+
+                return orders
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
     },
 
     Mutation: {
