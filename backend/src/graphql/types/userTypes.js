810d215abc2313aa2a4b4bb07b010aeefd45f9eb
morshedul-antor <antor.morshedul@gmail.com>
2024-05-08 11:50:38 +0600

    refactor: code alignment

--- backend/src/graphql/types/userTypes.js
+++ backend/src/graphql/types/userTypes.js
@@ -1,53 +1,53 @@
 const userTypeDefs = `
-  scalar DateTime
+    scalar DateTime
 
-  type Query {
-    users: [UserOut]
-    user(id:Int!): UserOut
-  }
+    type Query {
+        users: [UserOut]
+        user(id:Int!): UserOut
+    }
 
-  type Mutation {
-    createUser(data: UserIn!): UserOut
-    updateUser(id: Int!, data: UserUpdate): UserOut
-    login(data: Login!): Token
-  }
+    type Mutation {
+        createUser(data: UserIn!): UserOut
+        updateUser(id: Int!, data: UserUpdate): UserOut
+        login(data: Login!): Token
+    }
 
-  input UserIn {
-    firstName: String!
-    lastName: String!
-    email: String!
-    phone: String!
-    address: String!
-    password: String!
-  }
+    input UserIn {
+        firstName: String!
+        lastName: String!
+        email: String!
+        phone: String!
+        address: String!
+        password: String!
+    }
 
-  input UserUpdate {
-    firstName: String
-    lastName: String
-    email: String
-    phone: String
-    address: String
-  }
+    input UserUpdate {
+        firstName: String
+        lastName: String
+        email: String
+        phone: String
+        address: String
+    }
 
-  type UserOut {
-    firstName: String
-    lastName: String
-    email: String
-    phone: String
-    address: String
-    id: Int
-    createdAt: DateTime
-  }
+    type UserOut {
+        firstName: String
+        lastName: String
+        email: String
+        phone: String
+        address: String
+        id: Int
+        createdAt: DateTime
+    }
 
-  input Login {
-    identifier: String
-    password: String
-  }
+    input Login {
+        identifier: String
+        password: String
+    }
 
-  type Token {
-    type: String
-    token: String
-  }
+    type Token {
+        type: String
+        token: String
+    }
 `
 
 module.exports = {
