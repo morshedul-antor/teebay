b593d8b714f32ffd6607e6d9f38e4ee29e72037b
morshedul-antor <antor.morshedul@gmail.com>
2024-05-08 13:56:23 +0600

    feat: product resolvers add for product queries, product create, update & delete mutations

---
+++ backend/src/graphql/resolvers/productResolvers.js
@@ -0,0 +1,85 @@
+const { prismaClient } = require('../../db')
+
+const productResolvers = {
+    Query: {
+        products: async () => {
+            const products = await prismaClient.product.findMany({
+                include: {
+                    user: true,
+                },
+            })
+
+            for (const product of products) {
+                const categories = await prismaClient.category.findMany({
+                    where: {
+                        id: {
+                            in: product.categoryIds,
+                        },
+                    },
+                })
+                product.categories = categories
+            }
+
+            return products
+        },
+
+        product: async (_, { id }) => {
+            const product = await prismaClient.product.findUnique({
+                where: { id },
+                include: {
+                    user: true,
+                },
+            })
+
+            const categories = await prismaClient.category.findMany({
+                where: {
+                    id: {
+                        in: product.categoryIds,
+                    },
+                },
+            })
+            product.categories = categories
+
+            return product
+        },
+    },
+
+    Mutation: {
+        productCreate: async (_, { data }) => {
+            try {
+                return await prismaClient.product.create({
+                    data: data,
+                })
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
+
+        productUpdate: async (_, { id, data }) => {
+            try {
+                return await prismaClient.product.update({
+                    where: { id },
+                    data: data,
+                })
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
+
+        productDelete: async (_, { id }) => {
+            try {
+                const product = await prismaClient.product.delete({
+                    where: { id },
+                })
+
+                return `ID${product.id} - Product successfully deleted!`
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        },
+    },
+}
+
+module.exports = {
+    productResolvers,
+}
