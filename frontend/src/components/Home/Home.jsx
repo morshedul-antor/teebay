2466149ca46dc2e062b6fc1db70ca574a5e84428
morshedul-antor <antor.morshedul@gmail.com>
2024-05-09 18:11:58 +0600

    feat: users query fetch using apollo client

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,21 @@
 import classes from './Home.module.css'
+import { useQuery } from '@apollo/client'
+import { Link } from 'react-router-dom'
+import { queries } from '../../graphql'
 
 export default function Home() {
+    const { loading, error, data } = useQuery(queries.userQueries.GET_ALL_USERS)
+
+    if (loading) return <h1>Loading...</h1>
+    if (error) {
+        console.log(error)
+    }
+
     return (
         <div className={classes.home}>
             <h1>Teebay</h1>
+            {data.users && data.users.map((user, i) => <div key={i}>{user.firstName}</div>)}
+            <Link to="/dashboard">Dashboard</Link>
         </div>
     )
 }
