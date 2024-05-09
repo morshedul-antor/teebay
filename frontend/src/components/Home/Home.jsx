7a412122a860a9d0b7f205c3d5d0ce43f37db91b
morshedul-antor <antor.morshedul@gmail.com>
2024-05-09 11:34:32 +0600

    feat: home component add

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import classes from './Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={classes.home}>
+            <h1>Teebay</h1>
+        </div>
+    )
+}
