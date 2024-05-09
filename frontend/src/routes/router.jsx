764c86c9af906a1ccbb2f5faba4f7ea779755131
morshedul-antor <antor.morshedul@gmail.com>
2024-05-09 18:06:46 +0600

    import: login and dashboard route for path

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,10 +1,16 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import { HomePage } from '../pages'
+import { DashboardPage, LoginPage, HomePage } from '../pages'
+import ProtectedRoute from './ProtectedRoute'
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
             <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route path="/*" element={<ProtectedRoute />}>
+                <Route path="dashboard" element={<DashboardPage />} />
+            </Route>
         </Route>,
     ),
 )
