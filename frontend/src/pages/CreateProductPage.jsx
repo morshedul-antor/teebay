3586b2aa1e1a43c869a7f399c05785267f64b482
morshedul-antor <antor.morshedul@gmail.com>
2024-05-11 21:04:05 +0600

    feat: create product page add with navbar for routing

---
+++ frontend/src/pages/CreateProductPage.jsx
@@ -0,0 +1,10 @@
+import { CreateProduct, Navbar } from '../components'
+
+export default function CreateProductPage() {
+    return (
+        <div>
+            <Navbar />
+            <CreateProduct />
+        </div>
+    )
+}
