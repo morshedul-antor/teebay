13967c4da834e5a72f3d301b7597cb06a36cdbdd
morshedul-antor <antor.morshedul@gmail.com>
2024-05-11 20:24:07 +0600

    export: all atom components default export

---
+++ frontend/src/components/Atoms/index.js
@@ -0,0 +1,5 @@
+import Button from './Button/Button'
+import InputField from './InputField/InputField'
+import ErrorMessage from './ErrorMesage/ErrorMessage'
+
+export { Button, InputField, ErrorMessage }
