cefdb12d4e48a4b7b8d06db3de3c9848623114b3
morshedul-antor <antor.morshedul@gmail.com>
2024-05-11 20:13:18 +0600

    feat: all categories add

---
+++ frontend/src/utils/category.js
@@ -0,0 +1,8 @@
+export const categories = [
+    { id: 1, name: 'Electronics' },
+    { id: 2, name: 'Furnitures' },
+    { id: 3, name: 'Home Appliances' },
+    { id: 4, name: 'Sporting Goods' },
+    { id: 5, name: 'Outdoor' },
+    { id: 6, name: 'Toys' },
+]
