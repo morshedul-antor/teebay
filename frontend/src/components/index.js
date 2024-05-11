941ab342ffe58775cffe243f4de6d81295281b8e
morshedul-antor <antor.morshedul@gmail.com>
2024-05-11 21:09:33 +0600

    export: all components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,10 @@
 import Home from './Home/Home'
+import Navbar from './Navbar/Navbar'
 import Login from './Login/Login'
+import Register from './Register/Register'
 import Dashboard from './Dashboard/Dashboard'
+import MyProducts from './MyProducts/MyProducts'
+import SingleProduct from './SingleProduct/SingleProduct'
+import CreateProduct from './CreateProduct/CreateProduct'
 
-export { Home, Login, Dashboard }
+export { Home, Navbar, Login, Register, Dashboard, MyProducts, SingleProduct, CreateProduct }
