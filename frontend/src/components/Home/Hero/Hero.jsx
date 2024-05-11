c8047b9f35a199e2e26384d2d97f2b7d484ed99a
morshedul-antor <antor.morshedul@gmail.com>
2024-05-11 20:38:40 +0600

    feat: home component's hero section add

---
+++ frontend/src/components/Home/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import bg from '../../../assets/img/hero/bg.jpg'
+import hero from '../../../assets/img/hero/hero.png'
+import classes from './Hero.module.css'
+import { Button } from '../../Atoms'
+
+export default function Hero() {
+    return (
+        <div className={classes.hero} id="about">
+            <div className={classes.about}>
+                <div className={classes.box}>
+                    <div className={classes.title}>
+                        <p>
+                            Teebay <br />
+                        </p>
+                    </div>
+                    <p>Buy, Sell and Rent online</p>
+
+                    <div className={classes.btn}>
+                        <Button title="Shop Now &#8594;" size="18px" />
+                    </div>
+                </div>
+                <div className={classes.box}>
+                    <img src={bg} className={classes.bgImg} alt="" />
+                    <img src={hero} className={classes.img} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
