6b9e9c1658ec6936f5b76547d32812689f34290e
morshedul-antor <antor.morshedul@gmail.com>
2024-05-11 20:19:25 +0600

    feat: product list with product filter add by bought, sold, borrowed and lent tabs for specific user

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,28 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import classes from './Dashboard.module.css'
+import Tab from './Tab/Tab'
 
 export default function Dashboard() {
+    const tabs = ['bought', 'sold', 'borrowed', 'lent']
+    const [tab, setTab] = useState(1)
+
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <Link to="/">Home</Link>
+        <div className={classes.dash}>
+            <div className={classes.tab}>
+                {tabs.map((text, index) => (
+                    <p
+                        key={index}
+                        className={tab === index + 1 ? classes.active : ''}
+                        onClick={() => setTab(index + 1)}>
+                        {text}
+                    </p>
+                ))}
+            </div>
+
+            {tab === 1 && <Tab type="bought" />}
+            {tab === 2 && <Tab type="sold" />}
+            {tab === 3 && <Tab type="borrowed" />}
+            {tab === 4 && <Tab type="lent" />}
         </div>
     )
 }
