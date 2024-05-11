6b063f8f985c8b3cd9e01f3593d8c29fb230a700
morshedul-antor <antor.morshedul@gmail.com>
2024-05-11 20:35:11 +0600

    export: all queries as name queries

--- frontend/src/graphql/queries/index.js
+++ frontend/src/graphql/queries/index.js
@@ -1,4 +1,6 @@
 import * as userQueries from './userQueries'
 import * as categoryQueries from './categoryQueries'
+import * as productQueries from './productQueries'
+import * as orderQueries from './orderQueries'
 
-export { userQueries, categoryQueries }
+export { userQueries, categoryQueries, productQueries, orderQueries }
